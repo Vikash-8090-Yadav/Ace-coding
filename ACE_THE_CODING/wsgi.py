8b031af6120e2b2c0f81a9133f0925aa3c467d9f
vikash yadav <yadav621gzp@gmail.com>
2022-04-30 02:20:50 +0530

    a full website with the working template done in django

---
+++ ACE_THE_CODING/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ACE_THE_CODING project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ACE_THE_CODING.settings')
+
+application = get_wsgi_application()
