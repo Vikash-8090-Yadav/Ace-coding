85aca1870d5bb7f375e8ce2cfa638959fee2d1f4
vikash yadav <yadav621gzp@gmail.com>
2022-05-01 19:51:12 +0530

    All the urls for the privacy ,article and term's and condition mapped

--- ACE_THE_CODING/urls.py
+++ ACE_THE_CODING/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    path('',views.home),
+    path('terms-conditions/',views.term),
+    path('privacy-policy/',views.privacy),
+    path('article-details/',views.article),
+    # path('term/',views.term),
 ]
